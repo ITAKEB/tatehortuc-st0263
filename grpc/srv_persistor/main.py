b0ef1f37e16e8df2ca247e4bf1274c5fc2901296
ITAKEB <tomascf123@gmail.com>
2023-03-05 12:05:38 -0500

    Add grpc file-microservice

--- grpc/srv_persistor/main.py
+++ grpc/srv_persistor/main.py
@@ -1,21 +1,51 @@
 import grpc
+from os import listdir
+from concurrent import futures
+
 import records_pb2
 import records_pb2_grpc
 
-from concurrent import futures
+DataPath = "../../data"
 
-class Record(records_pb2_grpc.RecordServicer):
-    def PingRecords(self, request, context):
-        response = records_pb2.PingRecordResponse(ack='1')
+def ls(route):
+    return listdir(route)
+
+
+def searchFile(fileName):
+    files = ls(DataPath)
+    for file in files:
+        if fileName == file:
+            return True
+    return False
+
+
+class FileSystemServicer(records_pb2_grpc.FilesServiceServicer):
+    def ListFiles(self, request, context):
+        files = ls(DataPath)
+        response = records_pb2.ListFilesResponse(files=files)
+
         return response
+
+
+class FindFileSystemServicer(records_pb2_grpc.FindFilesServiceServicer):
+    def FindFiles(self, request, context):
+        name = request.name
+        found = searchFile(name)
+        response = records_pb2.FindFileResponse(found=found)
+
+        return response
+
 
 def main():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    records_pb2_grpc.add_RecordServicer_to_server(Record(), server)
+    records_pb2_grpc.add_FilesServiceServicer_to_server(
+        FileSystemServicer(), server)
+    records_pb2_grpc.add_FindFilesServiceServicer_to_server(
+        FindFileSystemServicer(), server)
 
     server.add_insecure_port('[::]:50051')
     server.start()
-    
+
     print('GRPC persistor server working')
     server.wait_for_termination()
 
--- grpc/srv_persistor/records_pb2_grpc.py
+++ grpc/srv_persistor/records_pb2_grpc.py
@@ -5,7 +5,7 @@ import grpc
 import records_pb2 as records__pb2
 
 
-class RecordStub(object):
+class FilesServiceStub(object):
     """Missing associated documentation comment in .proto file."""
 
     def __init__(self, channel):
@@ -14,53 +14,116 @@ class RecordStub(object):
         Args:
             channel: A grpc.Channel.
         """
-        self.PingRecords = channel.unary_unary(
-                '/Record/PingRecords',
-                request_serializer=records__pb2.EmptyMessage.SerializeToString,
-                response_deserializer=records__pb2.PingRecordResponse.FromString,
-                )
+        self.ListFiles = channel.unary_unary(
+            '/FilesService/ListFiles',
+            request_serializer=records__pb2.EmptyMessage.SerializeToString,
+            response_deserializer=records__pb2.ListFilesResponse.FromString,
+        )
 
 
-class RecordServicer(object):
+class FilesServiceServicer(object):
     """Missing associated documentation comment in .proto file."""
 
-    def PingRecords(self, request, context):
+    def ListFiles(self, request, context):
         """Missing associated documentation comment in .proto file."""
         context.set_code(grpc.StatusCode.UNIMPLEMENTED)
         context.set_details('Method not implemented!')
         raise NotImplementedError('Method not implemented!')
 
 
-def add_RecordServicer_to_server(servicer, server):
+def add_FilesServiceServicer_to_server(servicer, server):
     rpc_method_handlers = {
-            'PingRecords': grpc.unary_unary_rpc_method_handler(
-                    servicer.PingRecords,
-                    request_deserializer=records__pb2.EmptyMessage.FromString,
-                    response_serializer=records__pb2.PingRecordResponse.SerializeToString,
-            ),
+        'ListFiles': grpc.unary_unary_rpc_method_handler(
+            servicer.ListFiles,
+            request_deserializer=records__pb2.EmptyMessage.FromString,
+            response_serializer=records__pb2.ListFilesResponse.SerializeToString,
+        ),
     }
     generic_handler = grpc.method_handlers_generic_handler(
-            'Record', rpc_method_handlers)
+        'FilesService', rpc_method_handlers)
     server.add_generic_rpc_handlers((generic_handler,))
 
-
  # This class is part of an EXPERIMENTAL API.
-class Record(object):
+
+
+class FilesService(object):
     """Missing associated documentation comment in .proto file."""
 
     @staticmethod
-    def PingRecords(request,
-            target,
-            options=(),
-            channel_credentials=None,
-            call_credentials=None,
-            insecure=False,
-            compression=None,
-            wait_for_ready=None,
-            timeout=None,
-            metadata=None):
-        return grpc.experimental.unary_unary(request, target, '/Record/PingRecords',
-            records__pb2.EmptyMessage.SerializeToString,
-            records__pb2.PingRecordResponse.FromString,
-            options, channel_credentials,
-            insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
+    def ListFiles(request,
+                  target,
+                  options=(),
+                  channel_credentials=None,
+                  call_credentials=None,
+                  insecure=False,
+                  compression=None,
+                  wait_for_ready=None,
+                  timeout=None,
+                  metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/FilesService/ListFiles',
+                                             records__pb2.EmptyMessage.SerializeToString,
+                                             records__pb2.ListFilesResponse.FromString,
+                                             options, channel_credentials,
+                                             insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
+
+
+class FindFilesServiceStub(object):
+    """Missing associated documentation comment in .proto file."""
+
+    def __init__(self, channel):
+        """Constructor.
+
+        Args:
+            channel: A grpc.Channel.
+        """
+        self.FindFiles = channel.unary_unary(
+            '/FindFilesService/FindFiles',
+            request_serializer=records__pb2.NameMessage.SerializeToString,
+            response_deserializer=records__pb2.FindFileResponse.FromString,
+        )
+
+
+class FindFilesServiceServicer(object):
+    """Missing associated documentation comment in .proto file."""
+
+    def FindFiles(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
+
+def add_FindFilesServiceServicer_to_server(servicer, server):
+    rpc_method_handlers = {
+        'FindFiles': grpc.unary_unary_rpc_method_handler(
+            servicer.FindFiles,
+            request_deserializer=records__pb2.NameMessage.FromString,
+            response_serializer=records__pb2.FindFileResponse.SerializeToString,
+        ),
+    }
+    generic_handler = grpc.method_handlers_generic_handler(
+        'FindFilesService', rpc_method_handlers)
+    server.add_generic_rpc_handlers((generic_handler,))
+
+ # This class is part of an EXPERIMENTAL API.
+
+
+class FindFilesService(object):
+    """Missing associated documentation comment in .proto file."""
+
+    @staticmethod
+    def FindFiles(request,
+                  target,
+                  options=(),
+                  channel_credentials=None,
+                  call_credentials=None,
+                  insecure=False,
+                  compression=None,
+                  wait_for_ready=None,
+                  timeout=None,
+                  metadata=None):
+        return grpc.experimental.unary_unary(request, target, '/FindFilesService/FindFiles',
+                                             records__pb2.NameMessage.SerializeToString,
+                                             records__pb2.FindFileResponse.FromString,
+                                             options, channel_credentials,
+                                             insecure, call_credentials, compression, wait_for_ready, timeout, metadata)
